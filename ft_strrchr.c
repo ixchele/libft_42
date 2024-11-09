8b45fa5f39a3e403c4a05b475b0c40b517f24edb
Zakaria Bengueddime <zbengued@c3r4p4.1337.ma>
2024-11-09 11:32:55 +0100

    p

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: zbengued <zbengued@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 11:46:50 by zbengued          #+#    #+#             */
-/*   Updated: 2024/11/09 10:09:06 by zbengued         ###   ########.fr       */
+/*   Updated: 2024/11/09 11:31:20 by zbengued         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,14 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	size_t		i;
-	char		chr;
+	size_t	i;
 
 	i = ft_strlen(s);
-	chr = (char)c;
-	while (i > 0 && chr != s[i])
+	while (i + 1 > 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)s + i);
 		i--;
-	if (s[i] == chr)
-		return ((char *)s + 1);
-	else
-		return (NULL);
+	}
+	return (NULL);
 }
--- test.c
+++ test.c
@@ -4,12 +4,6 @@
 
 int main ()
 {
-	char s[] = "zakaria";
-	char d1[10];
-	char d2[10];
-
-	char *ptr1 = memmove(&s[2], s, 7);
-	char *ptr2 = ft_memmove(&s[2], s, 7);
-	printf("%s\n", ptr1);
-	printf("%s\n", ptr2);
+	char str[] = "zakaria wa3r";
+	printf("%s\n", ft_strrchr(str, '\0'));
 }
