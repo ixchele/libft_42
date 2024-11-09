802481845092c3d8a4090bf2b51d88db71370e0b
Zakaria Bengueddime <zbengued@c3r4p4.1337.ma>
2024-11-09 12:25:13 +0100

    p

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: zbengued <zbengued@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 14:26:48 by zbengued          #+#    #+#             */
-/*   Updated: 2024/11/09 10:01:24 by zbengued         ###   ########.fr       */
+/*   Updated: 2024/11/09 12:24:49 by zbengued         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	if (n == 0)
 		return (0);
 	i = 0;
-	while (s1[i] == s2[i] && i < n - 1)
+	while (s1[i] == s2[i] && i < n - 1 && (s1[i] && s2[i]))
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
