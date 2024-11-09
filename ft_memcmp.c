30472a8dc5f862abc9e638840dbd70658cf27fad
Zakaria Bengueddime <zbengued@c3r4p4.1337.ma>
2024-11-09 12:26:50 +0100

    p

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: zbengued <zbengued@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 15:23:10 by zbengued          #+#    #+#             */
-/*   Updated: 2024/11/09 12:15:13 by zbengued         ###   ########.fr       */
+/*   Updated: 2024/11/09 12:26:45 by zbengued         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	ps1 = (unsigned char *)s1;
 	ps2 = (unsigned char *)s2;
-	while (i < n && (ps1[i] || ps2[i]))
+	while (i < n)
 	{
 		if (ps1[i] != ps2[i])
 			return (ps1[i] - ps2[i]);
