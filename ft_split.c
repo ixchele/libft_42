7de9b963cf5c99ac7fdaf2293f5250f94e86e474
Zakaria Bengueddime <zbengued@c3r1p1.1337.ma>
2024-11-10 13:35:24 +0100

    p

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: zbengued <zbengued@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 15:21:55 by zbengued          #+#    #+#             */
-/*   Updated: 2024/11/10 13:09:43 by zbengued         ###   ########.fr       */
+/*   Updated: 2024/11/10 13:35:11 by zbengued         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ static size_t	count_word(char const *s, char c)
 	{
 		if (s[i] == c)
 			i++;
-		else 
+		else
 		{
 			count++;
 			while (s[i] && s[i] != c)
@@ -33,11 +33,32 @@ static size_t	count_word(char const *s, char c)
 	return (count);
 }
 
-static void	*ft_freer(char **tab, size_t i)
+static char	**splinter(char **tab, char *s, size_t i, size_t j)
 {
-    while (i > 0)
-		free(tab[--i]);
-	free(tab);
+	size_t	start;
+	size_t	word_len;
+
+	while (j < count_word(s, c))
+	{
+		word_len = 0;
+		while (s[i] == c && s[i])
+			i++;
+		start = i;
+		while (s[i] != c && s[i])
+		{
+			i++;
+			word_len++;
+		}
+		tab[j] = ft_substr(s, start, word_len);
+		if (!tab[j])
+		{
+			while (i > 0)
+				free(tab[--i]);
+			free(tab);
+		}
+		j++;
+	}
+	return (tab);
 }
 
 char	**ft_split(char const *s, char c)
@@ -48,28 +69,13 @@ char	**ft_split(char const *s, char c)
 	size_t	word_len;
 	size_t	start;
 
-	if (!s) 
+	if (!s)
 		return (NULL);
 	i = 0;
 	j = 0;
 	tab = ft_calloc(count_word(s, c) + 1, sizeof(char *));
 	if (!tab)
 		return (NULL);
-    while (j < count_word(s, c))
-    {
-		word_len = 0;
-        while (s[i] == c && s[i])
-            i++;
-		start = i;
-		while (s[i] != c && s[i])
-		{
-			i++;
-			word_len++;
-		}
-        tab[j] = ft_substr(s, start, word_len);
-        if (!tab[j])
-            ft_freer(tab, j);
-			j++;
-    }
+	tab = splinter(tab, s, i, j, word_len);
 	return (tab);
 }
