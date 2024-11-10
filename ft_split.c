b098cdc755e48130e6f2a5d887bdb4a48305c883
Zakaria Bengueddime <zbengued@c3r1p1.1337.ma>
2024-11-10 13:55:13 +0100

    p

--- ft_split.c
+++ ft_split.c
@@ -6,25 +6,20 @@
 /*   By: zbengued <zbengued@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 15:21:55 by zbengued          #+#    #+#             */
-/*   Updated: 2024/11/10 13:53:26 by zbengued         ###   ########.fr       */
+/*   Updated: 2024/11/10 13:55:09 by zbengued         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	count_word(char const *s, char c, int mode, size_t j)
+static size_t	count_word(char const *s, char c)
 {
 	size_t	i;
 	size_t	count;
 
 	i = 0;
 	count = 0;
-	while (s[j] != c && s[j] && mode == 1)
-	{
-		j++;
-		count++;
-	}
-	while (s[i] && mode == 0)
+	while (s[i])
 	{
 		if (s[i] == c)
 			i++;
@@ -40,14 +35,23 @@ static size_t	count_word(char const *s, char c, int mode, size_t j)
 
 static char	**splinter(char **tab, char const *s, size_t i, char c)
 {
+	size_t	start;
+	size_t	word_len;
 	size_t	j;
 
 	j = 0;
-	while (j < count_word(s, c, 0, 0))
+	while (j < count_word(s, c))
 	{
+		word_len = 0;
 		while (s[i] == c && s[i])
 			i++;
-		tab[j] = ft_substr(s, i, count_word(s, c, 1, i));
+		start = i;
+		while (s[i] != c && s[i])
+		{
+			i++;
+			word_len++;
+		}
+		tab[j] = ft_substr(s, start, word_len);
 		if (!tab[j])
 		{
 			while (i > 0)
@@ -67,7 +71,7 @@ char	**ft_split(char const *s, char c)
 	if (!s)
 		return (NULL);
 	i = 0;
-	tab = ft_calloc(count_word(s, c, 0, 0) + 1, sizeof(char *));
+	tab = ft_calloc(count_word(s, c) + 1, sizeof(char *));
 	if (!tab)
 		return (NULL);
 	tab = splinter(tab, s, i, c);
