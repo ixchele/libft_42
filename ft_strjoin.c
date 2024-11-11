e9569e0bdceec7c745cbc8d0cc72b6d6d90f73dc
Zakaria Bengueddime <zbengued@c3r5p2.1337.ma>
2024-11-11 10:17:55 +0100

    p

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: zbengued <zbengued@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 15:45:26 by zbengued          #+#    #+#             */
-/*   Updated: 2024/11/11 10:11:08 by zbengued         ###   ########.fr       */
+/*   Updated: 2024/11/11 10:17:50 by zbengued         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,13 +47,16 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*ptr;
+	size_t	ptr_size;
+
 
 	if (!s1 || !s2)
 		return (NULL);
-	ptr = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	ptr_size = ft_strlen(s1) + ft_strlen(s2) + 1
+	ptr = malloc(sizeof(char) * (ptr_size);
 	if (!ptr)
 		return (NULL);
-	ft_strlcpy(ptr, s1, ft_strlen(s1));
-	ft_strlcat(ptr, s2, ft_strlen(s2));
+	ft_strlcpy(ptr, s1, ptr_size);
+	ft_strlcat(ptr, s2, ptr_size);
 	return (ptr);
 }
