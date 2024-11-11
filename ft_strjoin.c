fc179d86e88407b0d65182491be3219f2a6772c2
Zakaria Bengueddime <zbengued@c3r5p2.1337.ma>
2024-11-11 10:09:00 +0100

    p

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,57 +6,54 @@
 /*   By: zbengued <zbengued@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 15:45:26 by zbengued          #+#    #+#             */
-/*   Updated: 2024/11/11 09:43:25 by zbengued         ###   ########.fr       */
+/*   Updated: 2024/11/11 10:08:54 by zbengued         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	char	*ft_strcat(char *s1, const char *s2)
-{
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	j = 0;
-	while (s1[i])
-		i++;
-	while (s2[j])
-	{
-		s1[i + j] = s2[j];
-		j++;
-	}
-	s1[i + j] = '\0';
-	return (s1);
-}
-
-static	char	*ft_cpy(const char *s1, char *dest)
-{
-	size_t	i;
-
-	i = 0;
-	while (s1[i])
-	{
-		dest[i] = s1[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
+// static	char	*ft_strcat(char *s1, const char *s2)
+// {
+// 	size_t	i;
+// 	size_t	j;
+//
+// 	i = 0;
+// 	j = 0;
+// 	while (s1[i])
+// 		i++;
+// 	while (s2[j])
+// 	{
+// 		s1[i + j] = s2[j];
+// 		j++;
+// 	}
+// 	s1[i + j] = '\0';
+// 	return (s1);
+// }
+//
+// static	char	*ft_cpy(const char *s1, char *dest)
+// {
+// 	size_t	i;
+//
+// 	i = 0;
+// 	while (s1[i])
+// 	{
+// 		dest[i] = s1[i];
+// 		i++;
+// 	}
+// 	dest[i] = '\0';
+// 	return (dest);
+// }
+//
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*ptr;
-	char	*cpy;
 
 	if (!s1 || !s2)
-		return (NULL);
-	cpy = malloc(sizeof (char) * (ft_strlen(s1) + 1));
-	if (!cpy)
 		return (NULL);
 	ptr = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!ptr)
 		return (NULL);
-	ptr = ft_strcat(ft_cpy(s1, cpy), s2);
+	ft_strlcpy(ptr, s1);
+	ft_strlcat(ptr, s2);
 	return (ptr);
 }
